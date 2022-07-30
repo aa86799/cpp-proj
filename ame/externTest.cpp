777352e2f38dc55f1d88d2111d203853faa2ce52
stone <aa86799@163.com>
2022-07-30 15:25:45 +0800

    测试 extern 关键字

---
+++ ame/externTest.cpp
@@ -0,0 +1,11 @@
+//
+// Created by stone on 2022/7/29.
+//
+
+extern const int bufSize;
+
+#include "iostream";
+using namespace std;
+void testBuf() {
+    cout << "bufSize:" << bufSize << endl;
+}
