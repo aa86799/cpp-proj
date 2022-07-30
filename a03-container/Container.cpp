9c996d7b488565276dd0f167d3368194c6ed0fa2
stone <aa86799@163.com>
2022-07-30 15:44:52 +0800

    const、list、static、initializer_list、assert 的应用

--- a03-container/Container.cpp
+++ a03-container/Container.cpp
@@ -3,12 +3,13 @@
 //
 
 #include <iostream>
-#include <vector>
+#include <vector> // 可变大小数组的序列容器；采用的连续存储空间来存储元素；动态分配数组
 #include <array>
 #include <list>
 #include <forward_list>
 #include <string>
 #include <functional>
+//#define NDEBUG // 如在程序顶部加上后，则不再执行内部的assert()
 
 using namespace std;
 
@@ -42,11 +43,11 @@ void testSwap() {
 
     cout << endl;
     array<int, 5> a1 = {1,2,3,4,5};
-    array<int, 5> a2 = {6, 7, 8};
-    swap(a1, a2);
-    cout << a1[0];
-    cout << a2[0];
-    cout << " " << a1.max_size();
+    array<int, 5> a2 = {6, 7, 8}; // 后面位置初始为0
+    swap(a1, a2); // 交换数组元素
+    cout << a1[4]; // 0
+    cout << a2[4]; // 5
+    cout << " maxsize: " << a1.max_size();
 
 //    forward_list<int> fl;
 //    fl.insert_after()
@@ -94,6 +95,54 @@ void testBind() {
     cout << "符合条件吗：" << (newCheck(3, "stone")) << endl;
 }
 
+void testConst() {
+    const int a = 1024;
+//    a= 8; // error
+
+//    const int *b = &a; // 常量指针，本质是指针，不论指向的是否是常量, *p 改变指向值不行.  底层
+    int const *b = &a; // 与上一句作用相同
+    int c = 1025;
+    b = &c;
+    cout << *b << endl; // 1025
+//    *b = 1026; // error 无法修改指向的值
+
+    int d = 1027;
+    const int *e = &d; // 常量指针，本质是指针，不论指向的是否是常量, *p 改变指向值不行.   底层
+    int f = 1025;
+    e = &f;
+    cout << *e << endl; // 1025
+//    *e = 1026; // error 无法修改指向的值
+    /* 以上这两种形式，就是 const 要在 指针符号 * 之前 */
+
+    int g = 1028;
+    int *const h = &g; // 指针常量。指针是常量；本质是一个常量，该常量是一个指针类型. 顶层 const
+    *h = 1029;
+//    h = &e; // error  无法修改指针地址
+
+    const int * const i = &g;
+    int const * const j = &g;
+
+//    int & const aa = 10; // error. 顶层 const 的引用常量， 不存在
+    const int &aa = 10; // 底层 const，常量引用
+}
+
+int staticArg() {
+    static int c = 10;
+    return ++c;
+}
+
+template<typename T>
+void testVarg(initializer_list<T> list) {
+    for (auto element: list) {
+        cout << element << endl;
+    }
+}
+
+int s1(int a[]) {return 0;}
+int s2(int* a) { return 1; }
+
+extern const int bufSize = 2046;
+#include "../ame/externTest.cpp" // 不需要 cmake 中添加
 int main() {
     testDifferenceType();
 
@@ -103,5 +152,26 @@ int main() {
 
     testBind();
 
+    testConst();
+
+    testBuf(); // in atest.cpp
+
+    list<int> li = {1,2,3};
+    for (auto i : li) {
+        cout << i << endl;
+    }
+
+    for (int i = 0; i < 10; ++i)
+        cout << staticArg() << endl;
+
+    testVarg(initializer_list<string>{"1,2,3", "cde"});
+
+    assert(true); // 断言，是一个宏；当为 false，则中断程序.
+    cout << "stone" << endl; // #define NDEBUG // 如在程序顶部加上后，则不再执行内部的assert()及之后的代码
+
+    cout << __TIME__ << endl;
+
     return 1;
 }
+
+
